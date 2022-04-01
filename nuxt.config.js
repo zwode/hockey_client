59da4b9612acf1e85e4a96463018efdfe31c5a7b
abiko <abiko2001@gmail.com>
2022-04-01 09:29:37 +0300

    upted

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'client_website',
+    title: 'TH Systems',
     htmlAttrs: {
       lang: 'en'
     },
